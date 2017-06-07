cbffeb02a18d3d64b08728bd86adf155353eb27d
Mokrane TIGUERCHA <mokrane.tiguercha@gmail.com>
2017-06-07 11:17:36 +0200

    COMMIT

---
+++ ALProjet2017AL/ALProjet2017AL/Models/Planning.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ALProjet2017AL.Models
+{
+    public class Planning
+    {
+        public string Class;
+        public string Promotion;
+        public string salle;
+    }
+}
