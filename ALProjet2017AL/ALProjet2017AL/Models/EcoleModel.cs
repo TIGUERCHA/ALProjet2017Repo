5c38c1f1a40d41f9937aa5d445074f7e53f7458f
Willy <ung.willybox@gmail.com>
2017-06-07 10:50:06 +0200

    Création classe école

---
+++ ALProjet2017AL/ALProjet2017AL/Models/EcoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ALProjet2017AL.Models
+{
+    public class EcoleModel
+    {
+        private String name { get; set; }
+        private AddressModel adresse { get; set; }
+        private String logoURL;
+    }
+}
