c6c9fd70720995756d75d78b22c6842e5d89b6a6
Mokrane TIGUERCHA <mokrane.tiguercha@gmail.com>
2017-06-07 12:43:58 +0200

    PLANNING CONTROLLER

---
+++ ALProjet2017AL/ALProjet2017AL/Controllers/PlanningController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ALProjet2017AL.Controllers
+{
+    public class PlanningController : Controller
+    {
+        // GET: Planning
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
