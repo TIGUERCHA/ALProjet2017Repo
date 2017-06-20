7898dc83f3e2fc4119e6504c56e863991ebe365f
Mokrane TIGUERCHA <mokrane.tiguercha@gmail.com>
2017-06-20 23:58:09 +0200

    commit

---
+++ ALProjet2017AL/ALProjet2017AL/Scripts/PlanningJs/Planning.js
@@ -0,0 +1,20 @@
+﻿
+//$(document).ready(initPage);
+
+//function initPage() {
+
+//    $('#calendar').datepicker({
+//        dateFormat: 'dd/mm/yy',
+//        inline: true,
+//        firstDay: 1,
+//        showOtherMonths: true,
+//        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//        onSelect: function (dateText, inst) {
+//            $('#form').val(dateText);
+//            alert("date recuperer : " + dateText);
+//            faireTraitement(dateText);
+    
+//        }
+    
+//    })
+//}
